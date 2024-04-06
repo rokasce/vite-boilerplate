74bfc00acfffcbbabaa14eba513981695b5f14c8
Rokas Cesnulevicius <rokasce@gmail.com>
2024-04-06 12:33:04 +0300

    Removed assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import { useState } from 'react';
 
-import reactLogo from './assets/react.svg';
 import { Button } from './components/ui/button';
 
 import './App.css';
-
-import viteLogo from '/vite.svg';
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
+    <div className='container'>
       <h1>Vite + React</h1>
       <div className='card'>
         <Button onClick={() => setCount((count) => count + 1)}>
@@ -29,10 +18,7 @@ const App = () => {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 };
 
